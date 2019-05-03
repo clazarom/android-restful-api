337ff44682b578f723ab65af3b5acbac76ca28e7
Caterina Lazaro <clazarom@hawk.iit.edu>
2019-05-03 15:24:29 -0500

    setup for JWT

---
+++ app/src/main/java/ece/iit/edu/sendrestrequest/JWT_REST_API/SecureRestClient.java
@@ -0,0 +1,12 @@
+package ece.iit.edu.sendrestrequest.JWT_REST_API;
+
+/**
+ * Secure Rest API client which encrypts every message using JWT
+ * https://jwt.io/
+ * https://github.com/auth0/java-jwt
+ *
+ * @author Caterina Lazaro
+ * @version 1.0 May 2019
+ */
+public class SecureRestClient {
+}
