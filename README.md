9efcbc153bbcc15fe85cf0afc06966efc7f0aa89
Cat <clazarom@hawk.iit.edu>
2019-05-14 12:03:34 -0500

    load apikey value with build.gradle

--- README.md
+++ README.md
@@ -5,3 +5,17 @@ The application will send the request to Postman
 ## Requirements
 - Android Studio 3.3
 - Android device or simulator: minsSDK = 24
+
+## REST API functions
+
+## JWT functions
+
+The application also sends some http request encrypted with JWT.
+
+To store the secrets:
+- First, we store the secrete key in a keystore 
+- The application can retrieve the keys from the keystore via gradle.
+https://medium.com/@geocohn/keeping-your-android-projects-secrets-secret-393b8855765d
+
+
+
--- app/src/main/java/ece/iit/edu/sendrestrequest/JWT_REST_API/SecureRestClient.java
+++ app/src/main/java/ece/iit/edu/sendrestrequest/JWT_REST_API/SecureRestClient.java
@@ -1,12 +1,16 @@
 package ece.iit.edu.sendrestrequest.JWT_REST_API;
 
+import ece.iit.edu.sendrestrequest.BuildConfig;
+
 /**
  * Secure Rest API client which encrypts every message using JWT
  * https://jwt.io/
  * https://github.com/auth0/java-jwt
+ * Hidding your api keys: https://medium.com/code-better/hiding-api-keys-from-your-android-repository-b23f5598b906
  *
  * @author Caterina Lazaro
  * @version 1.0 May 2019
  */
 public class SecureRestClient {
+    private String secretKey =  BuildConfig.apikey;
 }
