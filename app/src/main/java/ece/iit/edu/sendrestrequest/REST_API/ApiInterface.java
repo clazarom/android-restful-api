2027fb65adc8f36fdcde38e2a4a3490cba729d52
Caterina Lazaro <clazarom@hawk.iit.edu>
2019-05-14 20:39:16 -0500

    login to our server implemented
    debug: test the request with postman

--- app/src/main/java/ece/iit/edu/sendrestrequest/REST_API/ApiInterface.java
+++ app/src/main/java/ece/iit/edu/sendrestrequest/REST_API/ApiInterface.java
@@ -19,7 +19,7 @@ import retrofit2.http.Query;
 public interface ApiInterface {
 
     //ASYNCHRONOUS CALLS:
-    //{{base_urle}}/user/{n}?api_key=<<api_key>>&language=en-US
+    //{{base_url}}/user/{n}?api_key=<<api_key>>&language=en-US
     @GET("user/{user_n}")
     Single<UserCredentials> getUserDebug(
             @Path("user_n") int n,
